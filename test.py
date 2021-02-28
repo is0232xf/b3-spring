638c476c3a5e9837f9f25a1ca042953c33d0fd81
Yasuyuki Fujii <is0232xf@gmail.com>
2021-02-28 22:18:16 +0900

    [add] if-elif  rule

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ import math
 
 def for_loop(num):
     for i in range(num):
-        print(i)
+        if i % 2 == 0:
+            print('Even')
+            print(i)
+        elif i % 2 == 1:
+            print('Odd')
+            print(i)
         time.sleep(1)
 
 for_loop(5)
